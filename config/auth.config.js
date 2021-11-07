461350f1c9d35a354f52dabcbaf071b56d3dd627
burakergoren <burak.ergoren@sahibinden.com>
2021-11-08 00:54:17 +0300

    passport authanticate added & refactor

---
+++ config/auth.config.js
@@ -0,0 +1,32 @@
+var Q = require('q');
+const User = require('../models/user.js');
+var bcrypt = require('bcryptjs');
+
+
+module.exports = localAuth = function (username, password) {
+    var deferred = Q.defer();
+
+      User.findOne({'username' : username})
+        .then(function (result) {
+          if (null == result) {
+            console.log("USERNAME NOT FOUND:", username);
+  
+            deferred.resolve(false);
+          }
+          else {
+            var hash = result.password;
+            console.log("hash password :" + hash);
+            console.log("password :" + password);
+            console.log("FOUND USER: " + result.username);
+  
+            if (bcrypt.compareSync(password, hash)) {
+              deferred.resolve(result);
+            } else {
+              console.log("AUTHENTICATION FAILED");
+              deferred.resolve(false);
+            }
+          }
+        });
+
+        return deferred.promise;
+    };
---
+++ config/db.config.js
@@ -0,0 +1,24 @@
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+// uri from mongo atlas cloud db
+var  uri = "mongodb+srv://buraker:nodejsdemo@cluster0.5otud.mongodb.net/mongoDbTest?retryWrites=true&w=majority"
+
+// Declare a variable named option and assign optional settings
+const  options = {
+    useNewUrlParser:  true,
+    useUnifiedTopology:  true
+};  
+
+// Connect MongoDB Atlas using mongoose connect method
+mongoose.connect(uri, options).then(() => {
+    console.log("Database connection established!");
+},
+err  => {
+{
+    console.log("Error connecting Database instance due to:", err);
+}
+});
+
+module.exports = mongoose;
